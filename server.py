3804dd883643a168c674d0784e0e7b26246c6bda
Jay Toor <jay@rolepoint.com>
2013-06-17 16:58:10 +0100

    HTML and CSV data rendering.

--- server.py
+++ server.py
@@ -2,6 +2,11 @@
 
 from twisted.web import server, resource
 from twisted.internet import reactor
+from json import loads
+
+# Should store values in a database, 
+# but for test purposes using in memory dictionary.
+cache = {}
 
 class Test(resource.Resource):
   def getChild(self, name, request):
@@ -13,20 +18,34 @@ class Test(resource.Resource):
       return '<html><body><form method="POST"><input name="the-field" type="text"/></form></body></html>'
 
   def render_POST(self, request):
-      print "--->Fred"
-      newdata = request.content.getvalue()
-      print newdata
-      return '<html><h1>Test.POST</h1></html>'
+      raw_data = request.content.getvalue()
+      data = loads(raw_data)
+      
+      for data_raw_value in data.values():
+        data_value = loads(data_raw_value)
+        for key, value in data_value.iteritems():
+          cache[key] = value
+
+      return '<html><h1>SAVED</h1></html>'
 
 class TestHTML(resource.Resource):
   isLeaf=True
   def render_GET(self, request):
-      return "<html><body>TestHTML.render_GET</body></html>"
+      result = "<html><body>"
+      for k, v in cache.iteritems():
+        result += "%s %s<br/>" % (k,v)
+      result += "</body></html>"
+    
+      return str(result)
 
 class TestCSV(resource.Resource):
   isLeaf=True
   def render_GET(self, request):
-      return "<html><body>TestCSV.render_GET</body></html>"
+      result = ""
+      for k, v in cache.iteritems():
+        result += "%s,%s," % (k,v)
+
+      return str(result)
 
 blog_obj=Test()
 blog_obj.putChild('HTML', TestHTML())
